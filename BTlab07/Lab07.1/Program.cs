98b239ca9f07ca4878483d032cd290ca9ebc487f
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2024-10-24 10:14:06 +0700

    2410

---
+++ BTlab07/Lab07.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using DongVat.AnCo;
+using DongVat.AnThit;
+using System;
+
+
+
+namespace DongVat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bo bo = new Bo(1, "Bo", 500);
+            Trau trau = new Trau(2, "Trau", 600);
+            De de = new De(3, "De", 60);
+
+            CaSau caSau = new CaSau(4, "Ca Sau", 300);
+            Ho ho = new Ho(5, "Ho", 200);
+            SuTu suTu = new SuTu(6, "Su Tu", 220);
+
+            // sử dụng phương thức để hiển thị thông tin
+            Console.WriteLine(bo.GetInfo());
+            Console.WriteLine(trau.GetInfo());
+            Console.WriteLine(de.GetInfo());
+
+            Console.WriteLine(caSau.GetInfo());
+            Console.WriteLine(ho.GetInfo());
+            Console.WriteLine(suTu.GetInfo());
+        }
+    }
+}
