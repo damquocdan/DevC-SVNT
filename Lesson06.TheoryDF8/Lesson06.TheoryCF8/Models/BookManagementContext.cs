0db19ec6c400f2e90c75297c42ccb49610f511a8
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2024-11-18 17:33:58 +0700

    18/11

--- Lesson06.TheoryDF8/Lesson06.TheoryCF8/Models/BookManagementContext.cs
+++ Lesson06.TheoryDF8/Lesson06.TheoryCF8/Models/BookManagementContext.cs
@@ -7,6 +7,7 @@ namespace Lesson06.TheoryCF8.Models
         public BookManagementContext(DbContextOptions<BookManagementContext> options) : base(options) { }
         public DbSet<Book> Books { get; set;}
         public DbSet<Category> Categories { get; set;}
+
         public DbSet<Publisher> Publishers { get; set;}
     }
 }
---
+++ NetCoreMVCLab07/NetCoreMVCLab07/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetCoreMVCLab07.Models
+{
+    [Table("Blogs")] 
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Image {  get; set; }
+    }
+}
