034c1bd2a2f79d077c5117d8f35f46934a7ca523
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2024-11-22 09:25:57 +0700

    22/11

---
+++ Lesson09.Lap/Lesson09.Lap/Areas/Admins/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson09.Lap.Areas.Admins.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lesson09.Lap/Lesson09.Lap/Models​/ProductImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson09.Lap.Models​;
+
+public partial class ProductImage
+{
+    public int Id { get; set; }
+
+    public int? Pid { get; set; }
+
+    public string? Image { get; set; }
+}
